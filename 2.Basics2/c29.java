50f5440fd9542f1b9513dd8c18a3072894ea57c2
sahilvish01 <sahilvish6542@gmail.com>
2026-04-23 12:30:48 +0530

    Using Printf in Java

---
+++ 2.Basics2/c29.java
@@ -0,0 +1,16 @@
+// Using Printf in Java
+
+class c29 {
+
+    public static void main(String[] args)
+    {
+        System.out.printf("Hundred : %d", 100);
+
+        float a = 1.2f;
+
+        int b = (int) a;
+
+        System.out.printf("\nValue : %d", b);
+    }
+    
+}
