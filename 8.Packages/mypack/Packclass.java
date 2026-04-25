7aae3c0b1938627f7b999368ef0924e845b37631
sahilvish01 <sahilvish6542@gmail.com>
2026-04-25 17:56:20 +0530

    Package

---
+++ 8.Packages/mypack/Packclass.java
@@ -0,0 +1,17 @@
+package mypack;
+// Package
+
+
+public class Packclass {
+    private int var1;
+
+    public void setvar1(int var1)
+    {
+        this.var1 = var1;
+    }
+
+    public void displayvar1()
+    {
+        System.out.println("The Stored Value : " + var1);
+    }
+}
